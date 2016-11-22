1441f42ed6ecc4eddbe266c18fe43e9482a05b10
Jakob Ranum <jakobranum@hotmail.com>
2016-11-22 11:47:48 +0100

    Setting up basic interface and creating text handling script

---
+++ ClientServerMini/Assets/Scripts/ClientScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class ClientScript : MonoBehaviour {
+    TcpClient client;
+    NetworkStream stream;
+    StreamReader reader;
+    StreamWriter writer;
+
+    public string IPAddress;
+    public string nickname;
+
+    public InputField IP;
+    public InputField nick;
+
+	void Start () {
+	}
+	
+//	void Update () {
+//	
+//	}
+
+
+    public void parseIPAddress(){
+        this.IPAddress = IP.text;
+        print(this.IPAddress);
+    }
+
+    public void parseNickname(){
+        this.nickname = nick.text;
+        print(this.nickname);
+    }
+}
